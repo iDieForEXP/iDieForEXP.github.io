09b9a0e7567f5dda6074e8253e3d1f8360cbb6cb
William Geddes <William Geddes>
2016-10-25 17:16:00 -0400

    Pushing up

---
+++ script.js
@@ -0,0 +1,18 @@
+//Standalone variables
+var numRoll;
+var numOpponentRoll;
+var numSuccessThreshold;
+var numFailureThreshold;
+
+//Variables whose values are put into textboxes by the user
+var numStrength;
+var numStamina;
+var numAgility;
+var numIntelligence;
+var numCharisma;
+var numPerception;
+var numCircumstantialFactors;
+var numOpponentAgility;
+var numOpponentPerception;
+
+//Boolean variables 
